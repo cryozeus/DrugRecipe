e4d3a70cb29016d7893959d645a884fe36b26095
cryozeus <yashbhardwaj911@gmail.com>
2021-08-04 15:10:30 +0530

    project init

---
+++ src/main/java/com/licious/DrugRecipe/DrugRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.licious.DrugRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licious/DrugRecipe/DrugRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licious.DrugRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
